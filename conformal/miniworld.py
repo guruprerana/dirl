bb9eec204910a97924034626af34217928896a1e
Guruprerana Shabadi <guruprerana@gmail.com>
2025-04-25 20:30:16 -0400

    risky miniworld base env

--- conformal/miniworld.py
+++ conformal/miniworld.py
@@ -1,7 +1,23 @@
+from typing import Any, List
 from gymnasium import spaces, utils
+import copy
 
+from miniworld.entity import Entity
 from miniworld.miniworld import MiniWorldEnv
 
-class RiskyMiniworld(MiniWorldEnv, utils.EzPickle):
-    pass
+class RiskyMiniworld(MiniWorldEnv):
+    def reset(self, *, seed=None, options=None):
+        if options and isinstance(options, dict):
+            if "state" in options:
+                self.set_env_state = options["state"]
+        else:
+            self.set_env_state = None
+        return super().reset(seed=seed, options=options)
+    
+    def step(self, action):
+        obs, reward, termination, truncation, info = super().step(action)
+        info["env_state"] = self.get_env_state()
+        return obs, reward, termination, truncation, info
 
+    def get_env_state(self) -> Any:
+        raise NotImplementedError
--- conformal/rl_task_graph.py
+++ conformal/rl_task_graph.py
@@ -1,11 +1,14 @@
-from typing import Any, Dict, List, Tuple
+from typing import Any, Callable, Dict, List, Tuple
 from conformal.nonconformity_score_graph import NonConformityScoreGraph
 
 
 class RLTaskGraph(NonConformityScoreGraph):
-    def __init__(self, spec_graph: List[List[Tuple[int, str]]], env_name: str):
+    def __init__(self, spec_graph: List[List[Tuple[int, str]]], env_fn: Callable):
         self.spec_graph = spec_graph
         adj_lists = [[v for (v, _) in edges] for edges in spec_graph]
         super().__init__(adj_lists)
 
+        self.env_fn = env_fn
         self.path_policies: Dict[Tuple[int], Any] = dict()
+        self.init_states: Dict[Tuple[int], List[Any]] = dict()
+
